e5cbe36e7fcaf88adbd0a397d7e4a8623e75fd1e
faiithal <johndumlao2355@gmail.com>
2024-10-17 12:48:29 +0800

    Added database migration

---
+++ database/migrations/2024_10_10_110357_create_projects_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('user_id');
+            $table->string('file_link');
+            $table->integer('file_extension'); //take note of this
+            $table->text('project_desc');
+            $table->string('file_thumbnail');
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('projects');
+    }
+};
